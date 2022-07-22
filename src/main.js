6392ddb339784ba43b8a279c5f691956242b29bb
root <root@Elnx6.localdomain>
2022-07-22 13:59:05 -0300

    2nd commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import router from './router'
+
+import axios from 'axios'
+Vue.prototype.$http = axios
+
+import store from './store'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
