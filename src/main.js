e6e04a3b9b0397d9891f1bb2b2a7e2acbdced033
root <avcesar@hotmail.com>
2022-07-27 16:26:28 +0000

    segundo commit

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
 import router from './router'
-
 import axios from 'axios'
+// import store from './store'
+
 Vue.prototype.$http = axios
 
-import store from './store'
+
 
 Vue.config.productionTip = false
 
